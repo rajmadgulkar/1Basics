aaa4c758bea9a014616a53cf389e2242eeedd555
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-19 20:56:59 +0530

    Initial Commit

---
+++ Data Structures/Introduction to DS/a.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+struct CricPlayer{
+        int jerNo;
+        char pName[20];
+        float sal;
+}obj1={18,"Virat",5.0f};
+void main(){
+        struct CricPlayer obj2;
+        obj2.jerNo=45;
+        strcpy(obj2.pName,"Rohit");
+        obj2.sal=4.0f;
+
+        printf("%d\n",obj1.jerNo);
+        printf("%s\n",obj1.pName);
+        printf("%f\n",obj1.sal);
+
+        printf("%d\n",obj1.jerNo);
+        printf("%s\n",obj2.pName);                                                  printf("%f\n",obj2.sal);
+}
