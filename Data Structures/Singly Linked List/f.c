e504ee0a7319df88553b21bf61b4c4ceeb17ccd9
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-21 22:32:52 +0530

    Initial Commit

--- Data Structures/Singly Linked List/f.c
+++ Data Structures/Singly Linked List/f.c
@@ -15,13 +15,23 @@ void main(){
     newNode=(struct node*)malloc(sizeof(struct node));
     newNode->data=20;
     newNode->next=NULL;
+
     head->next=newNode;
 
     newNode=(struct node*)malloc(sizeof(struct node));
     newNode->data=30;
     newNode->next=NULL;
-    head->next->next=newNode;//Formation of Linked List code.
+    head->next->next=newNode;
 
-    
-    
+    struct node *temp=head;
+    while(temp!=NULL){
+        printf("%d\n",temp->data);
+        temp=temp->next;
+
+    }
 }
+/*
+10
+20
+30
+*/
