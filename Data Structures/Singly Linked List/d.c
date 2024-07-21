d023a4d659efda47506c6aa0bc30762483b0dd40
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-21 21:56:29 +0530

    Initial Commit

---
+++ Data Structures/Singly Linked List/d.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+struct node{
+    int data;
+    struct node *next;
+};
+void main(){
+    struct node *head=NULL;
+    struct node *newNode=(struct node*)malloc(sizeof(struct node));
+    newNode->data=10;
+    newNode->next=NULL;
+    head=newNode;
+
+    newNode=(struct node*)malloc(sizeof(struct node));
+    newNode->data=20;
+    newNode->next=NULL;
+    head->next=newNode;
+
+    newNode=(struct node*)malloc(sizeof(struct node));
+    newNode->data=30;
+    newNode->next=NULL;
+    head->next->next=newNode;//Formation of Linked List code.
+
+    
+    
+}
