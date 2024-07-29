4fe9ba6f5e24718719454c6a9146c98817383b26
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-29 22:25:24 +0530

    Initial Commit

---
+++ Day38/u.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+struct book {
+    char bName[20];
+    char authorName[20];
+    int pages;
+    double price;
+};
+
+void main() {
+    struct book bk = {"OS", "AG", 350, 450.50};
+    struct book bk1 = {"OS", "AG", 350, 450.50};
+
+    printf("%d\n",sizeof(bk));
+}
