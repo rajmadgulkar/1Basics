7e313633497bc5b04e70d5f8791532571aa816d1
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-29 22:33:27 +0530

    Initial Commit

---
+++ Day38/x.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+enum partners{
+    atharv,
+    raj,
+    ashwin,
+    prathamesh,
+    jay,
+    pratik,
+    atharva,
+    tejas
+};
+void main(){
+
+    printf("%d\n",atharv);
+    printf("%d\n",raj);
+    printf("%d\n",ashwin);
+    printf("%d\n",jay);
+    printf("%d\n",tejas);
+    printf("%d\n",atharva);
+
+}
+/*
+0
+1
+2
+4
+7
+6
+*/
