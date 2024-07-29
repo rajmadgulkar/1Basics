98b6c272fc4237f4d1c2b6b257098ba01055c18b
rajmadgulkar <rajmadgulkar1007@gmail.com>
2024-07-29 22:30:22 +0530

    Initial Commit

--- Day38/u.c
+++ Day38/u.c
@@ -13,3 +13,4 @@ void main() {
 
     printf("%d\n",sizeof(bk));
 }
+//56
---
+++ Day38/v.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+enum partners{
+    atharv,
+    raj,
+    ashwin,
+    prathamesh,
+    jay,
+    pratik,
+    atharva,
+    tejas
+};
+void main(){
+    enum partners obj;
+    printf("%d\n",sizeof(obj));
+
+}
+//4
